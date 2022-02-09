93f1f86e64a672f794c9a408562749c3bb8e14c0
amagicbarker@gmail.com <amagicbarker@gmail.com>
2022-02-08 18:37:17 -0700

    I am pushing this out to test out git

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1;
+            int number2;
+            int sum;
+            Console.WriteLine("Enter First Interger: ");
+            number1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter Second Interger: ");
+            number2 = int.Parse(Console.ReadLine());
+
+            sum = number1 + number2;
+
+            Console.WriteLine($"Sum is {sum}");
+        }
+    }
+}

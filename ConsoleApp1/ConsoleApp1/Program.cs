d2d0cb303fa29a10ea5ecf66299ce3ea32c0f7af
amagicbarker@gmail.com <amagicbarker@gmail.com>
2022-02-08 19:27:48 -0700

    Updating the assignment

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -9,15 +9,25 @@ namespace ConsoleApp1
             int number1;
             int number2;
             int sum;
+            int Product;
+            int Difference;
+            int Quotient;
             Console.WriteLine("Enter First Interger: ");
             number1 = int.Parse(Console.ReadLine());
 
             Console.WriteLine("Enter Second Interger: ");
-            number2 = int.Parse(Console.ReadLine());
+           number2 = int.Parse(Console.ReadLine());
 
             sum = number1 + number2;
+            Product = number1 * number2;
+            Difference = number1 - number2;
+            Quotient = number1 / number2;
 
             Console.WriteLine($"Sum is {sum}");
+            Console.WriteLine($"Product is {Product}");
+            Console.WriteLine($"Difference is {Difference}");
+            Console.WriteLine($"Quotient is {Quotient}");
+
         }
     }
 }

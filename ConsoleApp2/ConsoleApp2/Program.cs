87e72af2175b72850f5a954f247cc928988cf166
amagicbarker@gmail.com <amagicbarker@gmail.com>
2022-02-09 19:22:30 -0700

    Uploading to Git for assignment: Project Deliverable 1- Git Repositories

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //R is radious
+            Console.WriteLine("Enter Radius");
+            double R = Convert.ToDouble(Console.ReadLine());
+            double Diameter = 2 * R;
+            double Circumference = 2 * Math.PI * R;
+            double Area = Math.PI * R * R;
+            Console.WriteLine("The Diameter is " + Diameter);
+            Console.WriteLine("The Circumference is " + Circumference);
+            Console.WriteLine("The Area is " + Area); 
+        }
+    }
+}
